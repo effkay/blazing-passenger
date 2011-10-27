a7285c1b73d636c5a15faf0fa7a210bf5803ff36
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-10-27 22:01:40 +0200

    use new logging interface

--- lib/blazing-passenger/recipes/passenger_kickstart.rb
+++ lib/blazing-passenger/recipes/passenger_kickstart.rb
@@ -7,13 +7,13 @@ class Blazing::Recipe::PassengerKickstart < Blazing::Recipe
     super target_options
 
     if @options && @options.has_key?(:url)
-      info "Kickstarting #{@options[:url]}"
+      logger.info "Kickstarting #{@options[:url]}"
       HTTParty.get(@options[:url])
     else
-      error "No URL was specified for Passenger Kickstart"
+      logger.error "No URL was specified for Passenger Kickstart"
     end
   rescue
-    error "Unable to perform a GET request on #{@options[:url]}"
+    logger.error "Unable to perform a GET request on #{@options[:url]}"
   end
 
 end
--- lib/blazing-passenger/recipes/passenger_restart.rb
+++ lib/blazing-passenger/recipes/passenger_restart.rb
@@ -4,7 +4,8 @@ class Blazing::Recipe::PassengerRestart < Blazing::Recipe
 
   def run(target_options = {})
     super target_options
-    info 'Restarting passenger'
+
+    logger.info 'Restarting passenger'
     Dir.mkdir('tmp') unless File.exists? 'tmp'
     system 'touch tmp/restart.txt'
   end
